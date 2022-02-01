05d32017679ce60f90701b9552e78c6bc16cd185
MikiyasA <mikiyasad86@gmail.com>
2022-02-01 13:12:33 -0800

    To fix error of task 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- script that computes the score average of all records in the table second_table
 -- return avarage of all rows
-SELECT AVG(score) AS avarage FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
