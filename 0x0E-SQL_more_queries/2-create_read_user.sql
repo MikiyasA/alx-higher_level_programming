9868c82ffbff385850f3244ab36204406a702c9f
MikiyasA <mikiyasad86@gmail.com>
2022-02-02 14:30:07 -0800

    To commit mandatory task of 0x0E

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_2 and the user user_0d_2.
+-- useing CREATE DATABASE and 	    USER command
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON localhost.* TO 'user_0d_2'@'localhost'
