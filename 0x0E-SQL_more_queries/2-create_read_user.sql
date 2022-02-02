bb81bc7993b74d61133241f991d2445a6c6121bd
MikiyasA <mikiyasad86@gmail.com>
2022-02-02 14:41:52 -0800

    To fix some errors

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,6 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2.
 -- useing CREATE DATABASE and 	    USER command
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON localhost.* TO 'user_0d_2'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
